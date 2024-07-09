f291081a073cd3f4b08351838c443107a0047b10
jeonghun <jhe.kim@samsung.com>
2024-07-10 08:55:35 +0900

    [feature] 21

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -3,4 +3,5 @@ from prime_factors import PrimeFactor
 
 class PrimeFactorTest(TestCase):
     def test_prime_factor_of_1(self):
-        pass
+        prime_factor = PrimeFactor()
+        self.assertEqual(prime_factor.of(1), [])
